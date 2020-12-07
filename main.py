1405cd4144b0c3308e8b0a85eb6b81136fe807be
Doan Thi Mai <maidoan2017@gmail.com>
2020-12-08 00:51:34 +0700

    fix bug api

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os.path
 from werkzeug.utils import secure_filename
 import predict_1_file_or_folder as predict
 
-UPLOAD_FOLDER = "E:/Python/TextClassifier/predict/"
+UPLOAD_FOLDER = "E:/Python/TextClassifier/"
 
 app = Flask(__name__)
 app.secret_key = "hello"
@@ -17,13 +17,10 @@ def home():
 def file():
     if request.method == "POST":
         text = request.files['file']
-        # print("text", text)
         if text.filename:
             filename = secure_filename(text.filename)
-            # print("filename", filename)
             text.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             text_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            # print("text", text_path)
             result = predict.predict_1_file(text_path)
             flash(result)
             return redirect(url_for("home"))
