237ab9cd6baa641eb7e5326af2eb15bf87a60216
Doan Thi Mai <maidoan2017@gmail.com>
2020-12-09 17:50:53 +0700

    return json data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session, flash
+from flask import Flask, redirect, url_for, render_template, request, session, flash, jsonify
 import os.path
 from werkzeug.utils import secure_filename
 import predict_1_file_or_folder as predict
@@ -22,8 +22,9 @@ def file():
             text.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             text_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             result = predict.predict_1_file(text_path)
-            flash(result)
-            return redirect(url_for("home"))
+            # flash(result)
+            # return redirect(url_for("home"))
+            return jsonify(result)
     else:
         return render_template("test.html")
         # flash("Predict: ", result)
@@ -33,8 +34,9 @@ def text():
     if request.method == "POST":
         text = request.form['text']
         result = predict.predict_by_text(text)
-        flash(result)
-        return redirect(url_for("home"))
+        # flash(result)
+        # return redirect(url_for("home"))
+        return jsonify(result)
     else:
         return render_template("test.html")
 
